22a501ce6b4fd0a0ceb9de6ade768d7cee47d220
EnvityStudio <thuanit.mta@gmail.com>
2017-06-04 08:53:37 +0700

    doing cart

--- MyWebsite/Controllers/BakeryCartController.cs
+++ MyWebsite/Controllers/BakeryCartController.cs
@@ -6,12 +6,13 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using System.Data.SqlClient;
 
 namespace MyWebsite.Controllers
 {
     public class BakeryCartController : Controller
     {
-       
+        private MyDbContext db = new MyDbContext();
         public ActionResult Add(int id)
         {
 
@@ -20,7 +21,7 @@ namespace MyWebsite.Controllers
                 Cart = new BakeryCart();
             ProductDAO dao = new ProductDAO();
             SAN_PHAM sp = dao.FindProductByID(id);
-            Cart.AddItem(sp.MaSP, sp.TenSP, 1, sp.DonGia,sp.HinhAnh);
+            Cart.AddItem(sp.MaSP, sp.TenSP, 1, sp.DonGia, sp.HinhAnh);
 
             Session["cart"] = Cart;
             return Redirect(Request.UrlReferrer.ToString());
@@ -29,16 +30,16 @@ namespace MyWebsite.Controllers
         {
             BakeryCart Cart = (BakeryCart)Session["cart"];
             List<ItemCart> ls = new List<ItemCart>();
-            if(Cart != null)
+            if (Cart != null)
             {
 
                 ls = Cart.list;
             }
             return View(ls);
         }
-        public ActionResult UpdateAmount(int id,int quanity)
+        public ActionResult UpdateAmount(int id, int quanity)
         {
-            BakeryCart cart=(BakeryCart)Session["cart"];
+            BakeryCart cart = (BakeryCart)Session["cart"];
             if (cart != null)
             {
                 cart.addAmount(id, quanity);
@@ -53,6 +54,34 @@ namespace MyWebsite.Controllers
                 Cart.Delete(ID);
             }
             return Redirect(Request.UrlReferrer.ToString());
+        }
+        public ActionResult Pay()
+        {
+            return View();
+        }
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Pay([Bind(Include = "TenKH,DiaChi,SDT,TrangThai")] KHACH_HANG kHACH_HANG)
+
+        {
+            if (ModelState.IsValid)
+            {
+                Object[] kh =
+                {
+                    new SqlParameter ("@tenKH",kHACH_HANG.TenKH),
+                    new SqlParameter ("@diaChi",kHACH_HANG.DiaChi),
+                    new SqlParameter ("@sdt",kHACH_HANG.SDT)
+                };
+                int? res = db.Database.SqlQuery<int>("GetLastMaKH @tenKH, @diaChi, @sdt",kh).SingleOrDefault();
+                if (res == null)
+                {
+
+                }
+               
+                db.SaveChanges();
+                return RedirectToAction("List");
+            }
+            return View(kHACH_HANG);
         }
     }
 }
